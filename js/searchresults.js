5097c6fcdf39c6da282f9b222484283ea73be9a8
Edensha <eden.shakarov@gmail.com>
2025-07-21 17:33:41 +0300

    Create searchresults.js

---
+++ js/searchresults.js
@@ -0,0 +1,16 @@
+  <script>
+    $(document).ready(function () {
+      $('#searchTerm').text(localStorage.getItem('searchQuery'));
+      $('.product').click(function () {
+        const product = {
+          name: $(this).data('name'),
+          price: $(this).data('price'),
+          size: $(this).data('size'),
+          closetOwner: JSON.parse($(this).attr('data-closet'))
+        };
+        localStorage.setItem('selectedProduct', JSON.stringify(product));
+        localStorage.setItem('closetOwner', JSON.stringify(product.closetOwner));
+        window.location.href = 'product.html';
+      });
+    });
+  </script>
