170e3568c94c4616ea95894e1f90382765f97fcd
Edensha <eden.shakarov@gmail.com>
2025-07-22 09:09:34 +0300

    Update searchresults.js

--- js/searchresults.js
+++ js/searchresults.js
@@ -1,16 +1,37 @@
-$(document).ready(function () {
-      $('#searchTerm').text(localStorage.getItem('searchQuery'));
-      $('.product').click(function () {
-        const product = {
-          name: $(this).data('name'),
-          price: $(this).data('price'),
-          size: $(this).data('size'),
-          closetOwner: JSON.parse($(this).attr('data-closet'))
-        };
-        localStorage.setItem('selectedProduct', JSON.stringify(product));
-        localStorage.setItem('closetOwner', JSON.stringify(product.closetOwner));
-        window.location.href = 'product.html';
-      });
-    });
+const term = localStorage.getItem("searchTerm") || "שמלה";
+document.getElementById("searchTerm").innerText = term;
 
+const results = [
+  {
+    name: "שמלה אדומה",
+    price: "₪120",
+    img: "../images/dress1.jpg",
+    owner: "הילה"
+  },
+  {
+    name: "שמלה שחורה",
+    price: "₪140",
+    img: "images/dress2.jpg",
+    owner: "שירה"
+  }
+];
+
+const container = document.getElementById("resultsContainer");
+
+results.forEach((item, index) => {
+  const div = document.createElement("div");
+  div.className = "item product";
+  div.innerHTML = `
+    <img src="${item.img}" alt="${item.name}">
+    <h4>${item.name}</h4>
+    <p>${item.price}</p>
+    <button class="btn" onclick="viewProduct(${index})">לצפייה בפריט</button>
+  `;
+  container.appendChild(div);
+});
+
+function viewProduct(i) {
+  localStorage.setItem("product", JSON.stringify(results[i]));
+  window.location.href = "product.html";
+}
     
